e8e861e9d50726f9e99899ac638365f4126d437e
Leo-Gabriel02 <leo.pc.gabs11@gmail.com>
2024-03-20 00:18:15 -0400

    initial

---
+++ 380_robot_code/Debug/Core/Src/main.d
@@ -0,0 +1,106 @@
+Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/main.h \
+ ../Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal.h \
+ ../Core/Inc/stm32g4xx_hal_conf.h \
+ ../Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_rcc.h \
+ ../Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_def.h \
+ ../Drivers/CMSIS/Device/ST/STM32G4xx/Include/stm32g4xx.h \
+ ../Drivers/CMSIS/Device/ST/STM32G4xx/Include/stm32g431xx.h \
+ ../Drivers/CMSIS/Include/core_cm4.h \
+ ../Drivers/CMSIS/Include/cmsis_version.h \
+ ../Drivers/CMSIS/Include/cmsis_compiler.h \
+ ../Drivers/CMSIS/Include/cmsis_gcc.h \
+ ../Drivers/CMSIS/Include/mpu_armv7.h \
+ ../Drivers/CMSIS/Device/ST/STM32G4xx/Include/system_stm32g4xx.h \
+ ../Drivers/STM32G4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ ../Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_rcc_ex.h \
+ ../Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_gpio.h \
+ ../Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_gpio_ex.h \
+ ../Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_dma.h \
+ ../Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_dma_ex.h \
+ ../Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_cortex.h \
+ ../Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_exti.h \
+ ../Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_flash.h \
+ ../Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_flash_ex.h \
+ ../Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_flash_ramfunc.h \
+ ../Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_i2c.h \
+ ../Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_i2c_ex.h \
+ ../Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_pwr.h \
+ ../Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_pwr_ex.h \
+ ../Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_tim.h \
+ ../Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_tim_ex.h \
+ ../Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_uart.h \
+ ../Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_uart_ex.h \
+ ../Drivers/BSP/Components/vl53l4cx/modules/VL53Lx_api.h \
+ ../Drivers/BSP/Components/vl53l4cx/modules/vl53lx_api_core.h \
+ ../Drivers/BSP/Components/vl53l4cx/porting/vl53lx_platform.h \
+ ../Drivers/BSP/Components/vl53l4cx/modules/vl53lx_ll_def.h \
+ ../Drivers/BSP/Components/vl53l4cx/porting/vl53lx_platform_user_config.h \
+ ../Drivers/BSP/Components/vl53l4cx/porting/vl53lx_platform_user_defines.h \
+ ../Drivers/BSP/Components/vl53l4cx/modules/vl53lx_error_codes.h \
+ ../Drivers/BSP/Components/vl53l4cx/porting/vl53lx_types.h \
+ ../Drivers/BSP/Components/vl53l4cx/modules/vl53lx_register_structs.h \
+ ../Drivers/BSP/Components/vl53l4cx/modules/vl53lx_register_map.h \
+ ../Drivers/BSP/Components/vl53l4cx/modules/vl53lx_hist_structs.h \
+ ../Drivers/BSP/Components/vl53l4cx/modules/vl53lx_ll_device.h \
+ ../Drivers/BSP/Components/vl53l4cx/modules/vl53lx_dmax_structs.h \
+ ../Drivers/BSP/Components/vl53l4cx/modules/vl53lx_error_exceptions.h \
+ ../Drivers/BSP/Components/vl53l4cx/porting/vl53lx_platform_log.h \
+ ../Drivers/BSP/Components/vl53l4cx/porting/vl53lx_platform_ipp_imports.h \
+ ../Drivers/BSP/Components/vl53l4cx/porting/vl53lx_platform_user_data.h \
+ ../Drivers/BSP/Components/vl53l4cx/modules/vl53lx_def.h \
+ ../Drivers/BSP/Components/vl53l4cx/modules/vl53lx_ll_def.h \
+ ../Drivers/BSP/Components/vl53l4cx/vl53l4cx.h \
+ ../Drivers/BSP/Components/vl53l4cx/modules/vl53lx_preset_setup.h
+../Core/Inc/main.h:
+../Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal.h:
+../Core/Inc/stm32g4xx_hal_conf.h:
+../Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_rcc.h:
+../Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_def.h:
+../Drivers/CMSIS/Device/ST/STM32G4xx/Include/stm32g4xx.h:
+../Drivers/CMSIS/Device/ST/STM32G4xx/Include/stm32g431xx.h:
+../Drivers/CMSIS/Include/core_cm4.h:
+../Drivers/CMSIS/Include/cmsis_version.h:
+../Drivers/CMSIS/Include/cmsis_compiler.h:
+../Drivers/CMSIS/Include/cmsis_gcc.h:
+../Drivers/CMSIS/Include/mpu_armv7.h:
+../Drivers/CMSIS/Device/ST/STM32G4xx/Include/system_stm32g4xx.h:
+../Drivers/STM32G4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+../Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_rcc_ex.h:
+../Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_gpio.h:
+../Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_gpio_ex.h:
+../Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_dma.h:
+../Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_dma_ex.h:
+../Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_cortex.h:
+../Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_exti.h:
+../Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_flash.h:
+../Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_flash_ex.h:
+../Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_flash_ramfunc.h:
+../Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_i2c.h:
+../Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_i2c_ex.h:
+../Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_pwr.h:
+../Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_pwr_ex.h:
+../Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_tim.h:
+../Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_tim_ex.h:
+../Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_uart.h:
+../Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_uart_ex.h:
+../Drivers/BSP/Components/vl53l4cx/modules/VL53Lx_api.h:
+../Drivers/BSP/Components/vl53l4cx/modules/vl53lx_api_core.h:
+../Drivers/BSP/Components/vl53l4cx/porting/vl53lx_platform.h:
+../Drivers/BSP/Components/vl53l4cx/modules/vl53lx_ll_def.h:
+../Drivers/BSP/Components/vl53l4cx/porting/vl53lx_platform_user_config.h:
+../Drivers/BSP/Components/vl53l4cx/porting/vl53lx_platform_user_defines.h:
+../Drivers/BSP/Components/vl53l4cx/modules/vl53lx_error_codes.h:
+../Drivers/BSP/Components/vl53l4cx/porting/vl53lx_types.h:
+../Drivers/BSP/Components/vl53l4cx/modules/vl53lx_register_structs.h:
+../Drivers/BSP/Components/vl53l4cx/modules/vl53lx_register_map.h:
+../Drivers/BSP/Components/vl53l4cx/modules/vl53lx_hist_structs.h:
+../Drivers/BSP/Components/vl53l4cx/modules/vl53lx_ll_device.h:
+../Drivers/BSP/Components/vl53l4cx/modules/vl53lx_dmax_structs.h:
+../Drivers/BSP/Components/vl53l4cx/modules/vl53lx_error_exceptions.h:
+../Drivers/BSP/Components/vl53l4cx/porting/vl53lx_platform_log.h:
+../Drivers/BSP/Components/vl53l4cx/porting/vl53lx_platform_ipp_imports.h:
+../Drivers/BSP/Components/vl53l4cx/porting/vl53lx_platform_user_data.h:
+../Drivers/BSP/Components/vl53l4cx/modules/vl53lx_def.h:
+../Drivers/BSP/Components/vl53l4cx/modules/vl53lx_ll_def.h:
+../Drivers/BSP/Components/vl53l4cx/vl53l4cx.h:
+../Drivers/BSP/Components/vl53l4cx/modules/vl53lx_preset_setup.h:
